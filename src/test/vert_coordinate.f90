8ff6c7375718c27daf1afdd747357d36b2c0f41c
Jiseon-Oh <ojs9294@gmail.com>
2020-04-03 13:45:14 +0900

    dz stretching modify

--- src/test/vert_coordinate.f90
+++ src/test/vert_coordinate.f90
@@ -8,6 +8,7 @@ contains
     real, intent(out), dimension(nz)   :: z_full
     real, intent(out), dimension(nz+1) :: z_half
     integer :: k
+    real, parameter :: dzr = 0.9
     real, dimension(nz) :: dz   ! delta z for z_half
 
     select case (grid_dz)
@@ -19,17 +20,27 @@ contains
             end do
             z_full = z_half(1:nz) + dz/2.
         case ("stretching_grid")
-            print*, "TODO! need to work..."
-            stop
-            ! do k = 1, nz
-            !     dz(k) = dz(k-1)*dzr
-            !     print*, dz(k)
-            ! end do
-            ! calc_ztop = dz(1)*(dzr-1)/(dzr-1)
+            dz(1) = (ztop*1000*(dzr-1))/(dzr**nz-1)
+!               print*, dz(1)
+             do k = 2, nz
+                 dz(k) = dz(1)*(dzr**(k-1))
+!                 print*, dz(k)
+             end do
+
+            z_half(1) = zbottom * 1000.
+             do k = 1, nz
+                z_half(k+1) = z_half(k) + dz(k)
+                z_full(k) = z_half(k)+dz(k)/2.
+                print*, dz(k), z_half(k+1)
+             end do
+             
+
+
+             ! calc_ztop = dz(1)*(dzr-1)/(dzr-1)
         case default
-            print*, "Not setup grid_dz option. &
-                     please check input.nml"
-            stop
+!            print*, "Not setup grid_dz option. &
+!                     please check input.nml"
+!            stop
     end select
 
     end subroutine compute_vert_coord
